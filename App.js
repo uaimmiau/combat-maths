05541382c0ca10f718d76498e2d63523cdb53ae0
uaimmiau <zoltanchivay2137@gmail.com>
2023-04-01 22:28:57 +0200

    czerwony kolor

--- App.js
+++ App.js
@@ -21,7 +21,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#ff1122",
     alignItems: "center",
     justifyContent: "center",
   },
